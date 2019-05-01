276c91879177579762cd63a0479a2713861c6ccd
Brendon Dulam <brendon.dulam@veoneer.com>
2019-05-01 12:12:49 +0900

    section 4: pokedex

---
+++ section4/first-app/src/Dog.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Dog extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  };
+  }
+  render() {
+    return (
+      <div>
+        <h1>Dog</h1>
+      </div>
+    );
+  }
+}
+
+export default Dog;
