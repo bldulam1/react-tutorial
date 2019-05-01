935702d9c6b3fa52bb038d2cffe544c8068ff55e
Brendon Dulam <brendon.dulam@veoneer.com>
2019-05-01 20:27:24 +0900

    pokedex

--- section4/pokedex/src/App.js
+++ section4/pokedex/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Pokegame from './Pokegame/Pokegame';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Pokegame/>
     </div>
   );
 }
---
+++ section4/pokedex/src/Pokecard/Pokecard.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './Pokecard.css';
+
+const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+class Pokecard extends Component {
+  render() {
+    const {id, name, type, exp} = this.props;
+    const imgSrc = `${POKE_API}${id}.png`;
+    return (
+      <div className="Pokecard">
+        <h1 className="Pokecard-title"> {name} </h1>
+        <div className="Pokecard-image">
+          <img src={imgSrc} alt={name}/>
+        </div>
+        <div className="Pokecard-data">
+          Type: {type}
+        </div>
+        <div className="Pokecard-data">
+          EXP: {exp}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Pokecard;
